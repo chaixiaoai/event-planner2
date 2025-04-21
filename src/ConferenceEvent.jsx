719af52552a6d17047cfe911f70b1a9a99c2a100
chaixiaoai <chaixiaoai@gmail.com>
2025-04-21 02:44:58 -0400

    add more sections

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -5,13 +5,15 @@ import QuantityControls from "./QuantityControls";
 import Navbar from "./Navbar";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import { decrementAvQuantity, incrementAvQuantity } from "./avSlice";
+import { toggleMealSelection } from "./mealsSlice";
 const ConferenceEvent = () => {
     const [showDetails, setShowDetails] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
+    const mealItems = useSelector((state) => state.meal);
     const dispatch = useDispatch();
-    const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
-
     
     const handleToggleItems = () => {
         console.log("handleToggleItems called");
@@ -27,13 +29,15 @@ const ConferenceEvent = () => {
     };
 
     const handleIncrementAvQuantity = (index) => {
+        dispatch(incrementAvQuantity(index));
     };
 
     const handleDecrementAvQuantity = (index) => {
+        dispatch(decrementAvQuantity(index));
     };
 
     const handleMealSelection = (index) => {
-       
+        dispatch(toggleMealSelection(index));
     };
 
     const getItemsFromTotalCost = () => {
@@ -47,14 +51,26 @@ const ConferenceEvent = () => {
     };
     const calculateTotalCost = (section) => {
         let totalCost = 0;
+        let items = null;
         if (section === "venue") {
-          venueItems.forEach((item) => {
-            totalCost += item.cost * item.quantity;
-          });
+            items = venueItems;
+        } else if( section === "av") {
+            items = avItems;
+        } else if (section === "meal") {
+            items = mealItems;
         }
+        items.forEach((item) => {
+            if(section !== "meal") {
+                totalCost += item.cost * item.quantity;
+            } else if (section === "meal" && item.selected){
+                totalCost += item.cost * numberOfPeople;
+            }
+        });
         return totalCost;
       };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
+    const mealTotalCost = calculateTotalCost("meal");
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -112,30 +128,53 @@ const ConferenceEvent = () => {
                             <h1> Add-ons Selection</h1>
                         </div>
                         <div className="addons_selection">
+                            {avItems.map((item, index) => (
+                                <div className="av_data venue_main" key={index}>
+                                    <div className="img">
+                                        <img src={item.img} alt={item.name} />
+                                    </div>
+                                    <div className="text">{item.name}</div>
+                                    <div>${item.cost}</div>
+                                    <QuantityControls 
+                                        quantity = {avItems[index].quantity}
+                                        onAdd  = {() => handleIncrementAvQuantity(index)}
+                                        onRemove = {() => handleDecrementAvQuantity(index)}
+                                        isAddDisabled = {false}
+                                        isRemoveDisabled = {avItems[index].quantity === 0}
+                                    />
+                                </div>
+                            ))}
 
                         </div>
-                        <div className="total_cost">Total Cost:</div>
+                        <div className="total_cost">Total Cost: ${avTotalCost}</div>
 
                     </div>
 
                     {/* Meal Section */}
-
                     <div id="meals" className="venue_container container_main">
-
                         <div className="text">
-
                             <h1>Meals Selection</h1>
                         </div>
 
                         <div className="input-container venue_selection">
-
+                            <label htmlFor="numberOfPeople"><h3>Number of people: </h3></label>
+                            <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                                onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                min="1"
+                            />
                         </div>
                         <div className="meal_selection">
-
+                            {mealItems.map((item, index) => (
+                                <div className="meal_item" key={index}>
+                                    <input type="checkbox" id={index} checked={item.selected}
+                                        onChange={() => handleMealSelection(index)}
+                                    />
+                                    <div className="text">{item.name}</div>
+                                    <div>${item.cost}</div>
+                                </div>
+                            ))}
                         </div>
-                        <div className="total_cost">Total Cost: </div>
-
-
+                        <div className="total_cost">Total Cost: ${mealTotalCost} </div>
                     </div>
                 </div>
                     ) 
--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,13 +1,19 @@
-// mealsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+    { name: 'Breakfast', cost: 50, selected: false },
+    { name: 'High Tea', cost: 25, selected: false },
+    { name: 'Lunch', cost: 65, selected: false },
+    { name: 'Dinner', cost: 70, selected: false },
+];
 
 export const mealsSlice = createSlice({
   name: 'meals',
-  initialState: [
-   
-  ],
+  initialState: initialState,
   reducers: {
     toggleMealSelection: (state, action) => {
+        const {payload: index} = action;
+        state[index].selected = !state[index].selected;
     },
   },
 });
